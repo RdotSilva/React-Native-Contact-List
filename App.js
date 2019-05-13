2f513a31270861204403e5fdde80e2b1a65c7301
Ryan Silva <RyandotSilva@gmail.com>
2019-05-13 18:53:11 -0400

    Add sort method

--- App.js
+++ App.js
@@ -8,16 +8,23 @@ import {
 	View
 } from 'react-native';
 import { Constants } from 'expo';
-import contacts from './contacts';
+import contacts, { compareNames } from './contacts';
 import Row from './Row';
 
 export default class App extends React.Component {
 	state = {
-		showContacts: false
+		showContacts: false,
+		contacts: contacts
 	};
 
 	toggleContacts = () => {
 		this.setState(prevState => ({ showContacts: !prevState.showContacts }));
+	};
+
+	sort = () => {
+		this.setState(prevState => ({
+			contacts: prevState.contacts.sort(compareNames)
+		}));
 	};
 
 	renderItem = obj => <Row {...obj.item} />;
