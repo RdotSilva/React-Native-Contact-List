b7049f4e53dcd67c312434ad199b41ab8b03027e
Ryan Silva <RyandotSilva@gmail.com>
2019-05-13 16:52:35 -0400

    Add toggle contacts Button

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
+				<Button title="toggle contacts" onPress={this.toggleContacts} />
 			</View>
 		);
 	}
