46b661036b558d31b4c5add11ec474efb508adc8
Ryan Silva <RyandotSilva@gmail.com>
2019-05-16 20:14:56 -0400

    Clean up main App component

--- App.js
+++ App.js
@@ -9,9 +9,6 @@ import {
 } from 'react-native';
 import { Constants } from 'expo';
 import contacts, { compareNames } from './contacts';
-import Row from './Row';
-import ContactList from './ContactList';
-import AddContactForm from './AddContactForm';
 
 export default class App extends React.Component {
 	state = {
@@ -27,38 +24,12 @@ export default class App extends React.Component {
 		}));
 	};
 
-	// Toggle visibility of contacts
-	toggleContacts = () => {
-		this.setState(prevState => ({ showContacts: !prevState.showContacts }));
-	};
-
-	// Toggle visibility of form
-	toggleForm = () => {
-		this.setState(prevState => ({ showForm: !prevState.showForm }));
-	};
-
-	// Sort contacts
-	sort = () => {
-		this.setState(prevState => ({
-			contacts: [...prevState.contacts].sort(compareNames)
-		}));
-	};
-
 	// item: {name: String, phone: String }
 	// renderItem = obj => <Row {...obj.item} />;
 	// renderItem = obj => <Row name={obj.item.name} phone={obj.item.phone} />
 
 	render() {
-		if (this.state.showForm) return;
-		return (
-			<View style={styles.container}>
-				<Button title="Toggle Contacts" onPress={this.toggleContacts} />
-				<Button title="Add Contact" onPress={this.toggleForm} />
-				{this.state.showContacts && (
-					<ContactList contacts={this.state.contacts} />
-				)}
-			</View>
-		);
+		return; //something
 	}
 }
 
