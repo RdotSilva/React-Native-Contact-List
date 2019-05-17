a868f66626bf9a060596bc40f78d295e731fe004
Ryan Silva <RyandotSilva@gmail.com>
2019-05-16 20:32:40 -0400

    Change ContactList component name

--- SectionListContacts.js
+++ SectionListContacts.js
@@ -7,7 +7,7 @@ const renderItem = ({ item }) => <Row {...item} />;
 
 const renderSectionHeader = obj => <Text>{obj.section.title}</Text>;
 
-const ContactList = props => {
+const SectionListContacts = props => {
 	const contactsByLetter = props.contacts.reduce((obj, contact) => {
 		const firstLetter = contact.name[0].toUpperCase();
 		return {
