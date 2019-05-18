8f859d45bfaef696efe134ea9420b7c246cfa91b
Ryan Silva <RyandotSilva@gmail.com>
2019-05-18 12:29:41 -0400

    Add processContact function

--- api.js
+++ api.js
@@ -1,3 +1,8 @@
+const processContact = contact => ({
+	name: `${contact.name.first} ${contact.name.last}`,
+	phone: contact.phone
+});
+
 export const fetchUsers = async () => {
 	const response = await fetch(
 		'https://randomuser.me/api/?results=50&nat=us'
