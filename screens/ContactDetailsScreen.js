dfc42eaebe233087e570681b956c4f39785c3a8a
Ryan Silva <RyandotSilva@gmail.com>
2019-05-17 20:20:21 -0400

    Add ContactDetailsScreen component

---
+++ screens/ContactDetailsScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+
+export default class ContactDetailsScreen extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Phone # coming</Text>
+				<Button title="Go to contact" />
+			</View>
+		);
+	}
+
+	_goToRandom = () => {
+		// todo
+	};
+}
