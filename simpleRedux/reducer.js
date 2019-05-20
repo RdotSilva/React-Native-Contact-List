5f3a666bd5bfd1be98f7ebe9d2c1cca3bf4b0031
Ryan Silva <RyandotSilva@gmail.com>
2019-05-20 18:21:51 -0400

    Update state with reducer

--- simpleRedux/reducer.js
+++ simpleRedux/reducer.js
@@ -2,3 +2,11 @@ const reducer = (state, update) => ({
 	...state,
 	...update
 });
+
+let state = {};
+
+state = reducer(state, { foo: 'foo' });
+state = reducer(state, { bar: 'bar' });
+state = reducer(state, { foo: 'baz' });
+
+console.log(state); // { foo: 'baz', bar: 'bar' }
