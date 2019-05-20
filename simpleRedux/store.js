4a3fadad0afaf5fc51f8d125727b4c98e68b1df1
Ryan Silva <RyandotSilva@gmail.com>
2019-05-20 18:30:15 -0400

    Add dispatch method

--- simpleRedux/store.js
+++ simpleRedux/store.js
@@ -3,14 +3,6 @@ const reducer = (state, update) => ({
 	...update
 });
 
-let state = {};
-
-state = reducer(state, { foo: 'foo' });
-state = reducer(state, { bar: 'bar' });
-state = reducer(state, { foo: 'baz' });
-
-console.log(state); // { foo: 'baz', bar: 'bar' }
-
 class Store {
 	constructor(reducer, initialState) {
 		(this.reducer = reducer), (this.state = initialState);
@@ -19,4 +11,16 @@ class Store {
 	getState() {
 		return this.state;
 	}
+
+	dispatch(update) {
+		this.state = this.reducer(this.state, update);
+	}
 }
+
+const store = new Store(reducer, { foo: 'foo' });
+
+store.dispatch({ foo: 'foo' });
+store.dispatch({ bar: 'bar' });
+store.dispatch({ foo: 'baz' });
+
+console.log(store.getState());
