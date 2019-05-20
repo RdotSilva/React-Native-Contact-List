1e73e9d22a6642ca5d8e5c2a1a70413af5974205
Ryan Silva <RyandotSilva@gmail.com>
2019-05-20 18:25:42 -0400

    Add Store class

---
+++ simpleRedux/store.js
@@ -0,0 +1,18 @@
+const reducer = (state, update) => ({
+	...state,
+	...update
+});
+
+let state = {};
+
+state = reducer(state, { foo: 'foo' });
+state = reducer(state, { bar: 'bar' });
+state = reducer(state, { foo: 'baz' });
+
+console.log(state); // { foo: 'baz', bar: 'bar' }
+
+class Store {
+	constructor(reducer, initialState) {
+		(this.reducer = reducer), (this.state = initialState);
+	}
+}
