1550f40ee1536773a2d76f605ba275e91004d64f
Ryan Silva <RyandotSilva@gmail.com>
2019-05-20 19:59:45 -0400

    Add store.js file

---
+++ redux/store.js
@@ -0,0 +1,26 @@
+class Store {
+	constructor(reducer, initialState) {
+		(this.reducer = reducer), (this.state = initialState);
+	}
+
+	getState() {
+		return this.state;
+	}
+
+	dispatch(update) {
+		this.state = this.reducer(this.state, update);
+	}
+}
+
+const reducer = (state, update) => ({
+	...state,
+	...update
+});
+
+const store = new Store(reducer);
+
+store.dispatch({ foo: 'foo' });
+store.dispatch({ bar: 'bar' });
+store.dispatch({ foo: 'baz' });
+
+console.log(store.getState()); // { foo: 'baz', bar: 'bar' }
