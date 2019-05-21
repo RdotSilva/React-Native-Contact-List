a0d153c9e6db79b3102473dc2b963016174e2acc
Ryan Silva <RyandotSilva@gmail.com>
2019-05-20 20:54:49 -0400

    Change number to phone

--- redux/store.js
+++ redux/store.js
@@ -8,9 +8,9 @@ const store = createStore(reducer);
 // store.dispatch(updateUser({ bar: 'bar' }));
 // store.dispatch(updateUser({ foo: 'baz' }));
 
-store.dispatch(addContact({ name: 'My Name', number: '12345' }));
-store.dispatch(addContact({ name: 'My Name 2', number: '54321' }));
-store.dispatch(addContact({ name: 'My Name 3', number: '55353' }));
+store.dispatch(addContact({ name: 'My Name', phone: '12345' }));
+store.dispatch(addContact({ name: 'My Name 2', phone: '54321' }));
+store.dispatch(addContact({ name: 'My Name 3', phone: '55353' }));
 
 // console.log(store.getState());
 
