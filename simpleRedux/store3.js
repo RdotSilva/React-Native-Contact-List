a432aba890c5b29e9f008b4ae681b73cc51085bf
Ryan Silva <RyandotSilva@gmail.com>
2019-05-21 19:28:56 -0400

    Add logInUser function

---
+++ simpleRedux/store3.js
@@ -0,0 +1,82 @@
+// Action Types
+const UPDATE_USER = 'UPDATE_USER';
+const UPDATE_CONTACT = 'UPDATE_CONTACT';
+
+class Store {
+	constructor(reducer, initialState) {
+		(this.reducer = reducer), (this.state = initialState);
+	}
+
+	getState() {
+		return this.state;
+	}
+
+	dispatch(update) {
+		this.state = this.reducer(this.state, update);
+	}
+}
+
+const contactReducer = (state, action) => {
+	if (action.type === UPDATE_CONTACT) {
+		return [...state, action.payload];
+	}
+	return state;
+};
+
+const userReducer = (state, action) => {
+	if (action.type === UPDATE_USER) {
+		return {
+			...state,
+			...action.payload
+		};
+	}
+	if (action.type === UPDATE_CONTACT) {
+		return {
+			...state,
+			prevContact: action.payload
+		};
+	}
+	return state;
+};
+
+const DEFAULT_STATE = { user: {}, contacts: [] };
+
+const reducer = (state, action) => ({
+	user: userReducer(state.user, action),
+	contacts: contactReducer(state.contacts, action)
+});
+
+//Action Creators
+const updateUser = update => ({
+	type: UPDATE_USER,
+	payload: update
+});
+
+const addContact = newContact => ({
+	type: UPDATE_CONTACT,
+	payload: newContact
+});
+
+// Async Action Creator
+const logInUser = () => dispatch => {
+	dispatch({ type: 'LOG_IN_SENT' });
+	fetch().then(() => {
+		dispatch({ type: 'LOG_IN_SUCCESS' });
+	});
+};
+
+logInUser(); // returns dispatch => {}
+
+const store = new Store(reducer, DEFAULT_STATE);
+
+store.dispatch(logInUser());
+
+store.dispatch(updateUser({ foo: 'foo' }));
+store.dispatch(updateUser({ bar: 'bar' }));
+store.dispatch(updateUser({ foo: 'baz' }));
+
+store.dispatch(addContact({ name: 'My Name', number: '12345' }));
+store.dispatch(addContact({ name: 'My Name 2', number: '54321' }));
+store.dispatch(addContact({ name: 'My Name 3', number: '55353' }));
+
+console.log(store.getState());
