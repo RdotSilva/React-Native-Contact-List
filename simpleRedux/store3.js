2f4660b87e8d9eaca393bad8ac950a797cbb2de4
Ryan Silva <RyandotSilva@gmail.com>
2019-05-21 19:37:11 -0400

    Add async login function

--- simpleRedux/store3.js
+++ simpleRedux/store3.js
@@ -1,3 +1,18 @@
+const login = async (username, password) => {
+	const response = await fetch('http://localhost:8000', {
+		method: 'POST',
+		headers: { 'content-type': 'application/json' },
+		body: JSON.stringify({ username, password })
+	});
+
+	if (response.ok) {
+		return true;
+	}
+
+	const errMessage = await response.text();
+	throw new Error(errMessage);
+};
+
 // Action Types
 const UPDATE_USER = 'UPDATE_USER';
 const UPDATE_CONTACT = 'UPDATE_CONTACT';
@@ -62,7 +77,7 @@ const addContact = newContact => ({
 });
 
 // Async Action Creator
-const logInUser = () => dispatch => {
+const logInUser = (username, password) => dispatch => {
 	dispatch({ type: 'LOG_IN_SENT' });
 	fetch()
 		.then(() => {
