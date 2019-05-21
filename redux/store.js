e05688db9de2c0e5686cf1ea96b63e47fbb32dc5
Ryan Silva <RyandotSilva@gmail.com>
2019-05-20 20:04:37 -0400

    Use createStore to make store

--- redux/store.js
+++ redux/store.js
@@ -1,26 +1,60 @@
-class Store {
-	constructor(reducer, initialState) {
-		(this.reducer = reducer), (this.state = initialState);
-	}
+// import { createStore } from 'redux';
+const { createStore } = require('redux');
 
-	getState() {
-		return this.state;
-	}
+// Action Types
+const UPDATE_USER = 'UPDATE_USER';
+const UPDATE_CONTACT = 'UPDATE_CONTACT';
 
-	dispatch(update) {
-		this.state = this.reducer(this.state, update);
-	}
-}
+// Init blank state
+const DEFAULT_STATE = { user: {}, contacts: [] };
 
-const reducer = (state, update) => ({
-	...state,
-	...update
+const contactReducer = (state, action) => {
+	if (action.type === UPDATE_CONTACT) {
+		return [...state, action.payload];
+	}
+	return state;
+};
+
+const userReducer = (state, action) => {
+	if (action.type === UPDATE_USER) {
+		return {
+			...state,
+			...action.payload
+		};
+	}
+	if (action.type === UPDATE_CONTACT) {
+		return {
+			...state,
+			prevContact: action.payload
+		};
+	}
+	return state;
+};
+
+const reducer = (state, action) => ({
+	user: userReducer(state.user, action),
+	contacts: contactReducer(state.contacts, action)
 });
 
-const store = new Store(reducer);
+//Action Creators
+const updateUser = update => ({
+	type: UPDATE_USER,
+	payload: update
+});
 
-store.dispatch({ foo: 'foo' });
-store.dispatch({ bar: 'bar' });
-store.dispatch({ foo: 'baz' });
+const addContact = newContact => ({
+	type: UPDATE_CONTACT,
+	payload: newContact
+});
 
-console.log(store.getState()); // { foo: 'baz', bar: 'bar' }
+const store = createStore(reducer, DEFAULT_STATE);
+
+store.dispatch(updateUser({ foo: 'foo' }));
+store.dispatch(updateUser({ bar: 'bar' }));
+store.dispatch(updateUser({ foo: 'baz' }));
+
+store.dispatch(addContact({ name: 'My Name', number: '12345' }));
+store.dispatch(addContact({ name: 'My Name 2', number: '54321' }));
+store.dispatch(addContact({ name: 'My Name 3', number: '55353' }));
+
+console.log(store.getState());
