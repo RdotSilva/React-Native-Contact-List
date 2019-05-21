62e959d9afea629f2374c33a76ca5fa92c489764
Ryan Silva <RyandotSilva@gmail.com>
2019-05-20 20:12:16 -0400

    Move action types and action creators

---
+++ redux/actions.js
@@ -0,0 +1,14 @@
+// Action Types
+export const UPDATE_USER = 'UPDATE_USER';
+export const UPDATE_CONTACT = 'UPDATE_CONTACT';
+
+//Action Creators
+export const updateUser = update => ({
+	type: UPDATE_USER,
+	payload: update
+});
+
+export const addContact = newContact => ({
+	type: UPDATE_CONTACT,
+	payload: newContact
+});
