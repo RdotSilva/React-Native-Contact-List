325e2606ab965c9159b1b3be4fbbbb0aa6d51db6
Ryan Silva <RyandotSilva@gmail.com>
2019-05-22 19:09:25 -0400

    Add log in action types

--- redux/actions.js
+++ redux/actions.js
@@ -1,6 +1,9 @@
 // Action Types
 export const UPDATE_USER = 'UPDATE_USER';
 export const UPDATE_CONTACT = 'UPDATE_CONTACT';
+export const LOG_IN_SENT = 'LOG_IN_SENT';
+export const LOG_IN_FULFILLED = 'LOG_IN_FULFILLED';
+export const LOG_IN_REJECTED = 'LOG_IN_REJECTED';
 
 //Action Creators
 export const updateUser = update => ({
