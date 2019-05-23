2b0ec9112aa5dedd82caefdb469538923247fdce
Ryan Silva <RyandotSilva@gmail.com>
2019-05-22 20:12:34 -0400

    Add persisted logic to store

--- redux/store.js
+++ redux/store.js
@@ -3,6 +3,16 @@ import { addContact } from './actions';
 import reducer from './reducer';
 import thunk from 'redux-thunk';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+	key: 'root',
+	storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 // const thunkMiddleware = store => next => action => {
 // 	if (typeof action === 'function') {
 // 		action(store.dispatch);
@@ -11,16 +21,15 @@ import thunk from 'redux-thunk';
 // 	}
 // };
 
-const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
 
 // store.dispatch(updateUser({ foo: 'foo' }));
 // store.dispatch(updateUser({ bar: 'bar' }));
 // store.dispatch(updateUser({ foo: 'baz' }));
 
-store.dispatch(addContact({ name: 'My Name', phone: '12345' }));
-store.dispatch(addContact({ name: 'My Name 2', phone: '54321' }));
-store.dispatch(addContact({ name: 'My Name 3', phone: '55353' }));
+// store.dispatch(addContact({ name: 'My Name', phone: '12345' }));
+// store.dispatch(addContact({ name: 'My Name 2', phone: '54321' }));
+// store.dispatch(addContact({ name: 'My Name 3', phone: '55353' }));
 
 // console.log(store.getState());
-
-export default store;
