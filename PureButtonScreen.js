8476a0d2254fa9343f7bcdbb7e873f804b96d48d
Ryan Silva <RyandotSilva@gmail.com>
2019-05-23 20:15:17 -0400

    Add style for PureButtonScreen component

--- PureButtonScreen.js
+++ PureButtonScreen.js
@@ -3,6 +3,12 @@ import { StyleSheet, View, Text } from 'react-native';
 
 import PureButton from './PureButton';
 
+const styles = StyleSheet.create({
+	button: {
+		alignSelf: 'center'
+	}
+});
+
 export default class PureButtonScreen extends React.Component {
 	state = {
 		count: 0
