f13245163b27646d0d2d6d6d9ee4bcf801bc073a
Ryan Silva <RyandotSilva@gmail.com>
2019-05-24 18:48:22 -0400

    Change updateUser to snapshot test

--- redux/actions.test.js
+++ redux/actions.test.js
@@ -2,8 +2,5 @@ import * as actions from './actions';
 import { exportAllDeclaration } from '@babel/types';
 
 test('updateUser returns an action', () => {
-	expect(actions.updateUser({ name: 'test name' })).toEqual({
-		type: actions.UPDATE_USER,
-		payload: { name: 'test name' }
-	});
+	expect(actions.updateUser({ name: 'test name' })).toMatchSnapshot();
 });
