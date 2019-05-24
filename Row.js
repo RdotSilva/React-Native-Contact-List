08d767b0489e1180f16c8d0be0a14609f27afdd4
Ryan Silva <RyandotSilva@gmail.com>
2019-05-23 20:03:02 -0400

    Change Row to PureComponent

--- Row.js
+++ Row.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
 	row: { padding: 20 }
 });
 
-const Row = props => (
-	<TouchableOpacity
-		style={styles.row}
-		onPress={() => {
-			props.onSelectContact(props);
-		}}
-	>
-		<Text>{props.name}</Text>
-		<Text>{props.phone}</Text>
-	</TouchableOpacity>
-);
+class Row extends React.PureComponent {
+	render() {
+		const { props } = this;
+		return (
+			<View style={styles.row}>
+				<Text>{props.name}</Text>
+				<Text>{props.phone}</Text>
+			</View>
+		);
+	}
+}
 
 Row.propTypes = {
 	name: PropTypes.string,
