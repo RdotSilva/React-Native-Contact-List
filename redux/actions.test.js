d04567ab70c585c3ef1ed62d52e18d84c8e1b175
Ryan Silva <RyandotSilva@gmail.com>
2019-05-24 18:50:23 -0400

    Add additional updateUser snapshot tests

--- redux/actions.test.js
+++ redux/actions.test.js
@@ -4,3 +4,11 @@ import { exportAllDeclaration } from '@babel/types';
 test('updateUser returns an action', () => {
 	expect(actions.updateUser({ name: 'test name' })).toMatchSnapshot();
 });
+
+test('updateUser returns an action when passed empty object', () => {
+	expect(actions.updateUser({})).toMatchSnapshot();
+});
+
+test('updateUser returns an action when passed empty name', () => {
+	expect(actions.updateUser({ name: '' })).toMatchSnapshot();
+});
