3d133717806747c98d6542fe0cb88bc018e6d960
Ryan Silva <RyandotSilva@gmail.com>
2019-05-25 09:22:08 -0400

    Add describe method to group tests

--- redux/actions.test.js
+++ redux/actions.test.js
@@ -1,14 +1,15 @@
 import * as actions from './actions';
-import { exportAllDeclaration } from '@babel/types';
 
-test('updateUser returns an action', () => {
-	expect(actions.updateUser({ name: 'test name' })).toMatchSnapshot();
-});
+describe('updateUser retruns actions', () => {
+	it('returns an action', () => {
+		expect(actions.updateUser({ name: 'test name' })).toMatchSnapshot();
+	});
 
-test('updateUser returns an action when passed empty object', () => {
-	expect(actions.updateUser({})).toMatchSnapshot();
-});
+	it('handles empty object', () => {
+		expect(actions.updateUser({})).toMatchSnapshot();
+	});
 
-test('updateUser returns an action when passed empty name', () => {
-	expect(actions.updateUser({ name: '' })).toMatchSnapshot();
+	it('handles emptyy name', () => {
+		expect(actions.updateUser({ name: '' })).toMatchSnapshot();
+	});
 });
