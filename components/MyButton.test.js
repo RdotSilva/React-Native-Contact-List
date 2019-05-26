1164ce790864be8147b7d992538229eac7199020
Ryan Silva <RyandotSilva@gmail.com>
2019-05-26 10:06:21 -0400

    Update override default color test

--- components/MyButton.test.js
+++ components/MyButton.test.js
@@ -1,6 +1,10 @@
 import MyButton from './MyButton';
+import { Button } from 'react-native';
 import renderer from 'react-test-renderer';
 import React from 'react';
+
+const getUnderlyingButton = testInstance =>
+	testInstance.root.findByType(Button);
 
 describe('MyButton', () => {
 	it('renders', () => {
@@ -13,5 +17,10 @@ describe('MyButton', () => {
 			renderer.create(<MyButton color={color} />)
 		);
 		expect(button.props.color).toBe(color);
+		const color2 = 'blue';
+		const button2 = getUnderlyingButton(
+			renderer.create(<MyButton color={color2} />)
+		);
+		expect(button2.props.color).toBe(color2);
 	});
 });
