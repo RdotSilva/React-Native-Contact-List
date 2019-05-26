faf4de5973790b1f71062cb7de3b7a494f955c7e
Ryan Silva <RyandotSilva@gmail.com>
2019-05-26 09:31:45 -0400

    Add default state

--- redux/reducer.test.js
+++ redux/reducer.test.js
@@ -1,6 +1,11 @@
 import reducer from './reducer';
 import * as actions from './actions';
 
+const DEFAULT_STATE = {
+	user: {},
+	contacts: []
+};
+
 describe('contact reducer', () => {
 	it('successfully adds new user', () => {});
 });
